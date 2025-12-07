2762704e95b5bc24d44c2b958e43382211c937e8
mingihon <MingiHong32@gmail.com>
2025-12-06 19:26:17 -0500

    made token obtaining and email flow more smoothly in one go, will work to implement with UI later

--- get_email.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-access_token = "insert_access_token_here"
-url = "https://graph.microsoft.com/v1.0/me/messages"
-headers = {"Authorization": "Bearer " + access_token, "Host": "graph.microsoft.com"}
-response = requests.get(url, headers=headers)
-print(response.text)
--- get_token_and_emails.py
+++ get_token_and_emails.py
@@ -1,5 +1,6 @@
 import msal
 from msal import PublicClientApplication
+import requests
 
 client_id = "f7e1e1be-526d-458a-bcef-b377558bc4ef"
 authority = "https://login.microsoftonline.com/common/"
@@ -21,5 +22,16 @@ if not result:
     result = app.acquire_token_interactive( 
          scopes=scope)
     
-print(result)
+#print(result)
+#print(result.keys())
+
+if "error" in result.keys():
+    print("error, please try again")
+else:
+    access_token = result['access_token']
+    url = "https://graph.microsoft.com/v1.0/me/messages"
+    headers = {"Authorization": "Bearer " + access_token, "Host": "graph.microsoft.com", 
+               "Prefer": 'outlook.body-content-type="text"'}
+    response = requests.get(url, headers=headers)
+    print(response.text)
 
